0b83b0956369fe2d97932a3adc17a3460d5f9e31
codezzzsleep <codezzzsleep@163.com>
2023-07-28 16:03:39 +0800

    add

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,9 +1,12 @@
 <template>
-	<view class="container">
-		
-		<view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
-		<text class="intro">详见：</text>
-		<uni-link :href="href" :text="href"></uni-link>
+	<view class="content">
+		<!-- #ifdef APP-PLUS || H5 -->
+		<view @click="echarts.onClick" :prop="option" :change:prop="echarts.updateEcharts" id="echarts" class="echarts"></view>
+		<button @click="changeOption">更新数据</button>
+		<!-- #endif -->
+		<!-- #ifndef APP-PLUS || H5 -->
+		<view>非 APP、H5 环境不支持</view>
+		<!-- #endif -->
 	</view>
 </template>
 
@@ -11,19 +14,90 @@
 	export default {
 		data() {
 			return {
-				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+				option: {
+					title: {
+						text: 'ECharts 入门示例'
+					},
+					tooltip: {},
+					legend: {
+						data: ['销量']
+					},
+					xAxis: {
+						data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+					},
+					yAxis: {},
+					series: [{
+						name: '销量',
+						type: 'bar',
+						data: [5, 20, 36, 10, 10, 20]
+					}]
+				}
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			changeOption() {
+				const data = this.option.series[0].data
+				// 随机更新示例数据
+				data.forEach((item, index) => {
+					data.splice(index, 1, Math.random() * 40)
+				})
+			},
+			onViewClick(options) {
+				console.log(options)
+			}
+		}
+	}
+</script>
+
+<script module="echarts" lang="renderjs">
+	let myChart
+	export default {
+		mounted() {
+			if (typeof window.echarts === 'function') {
+				this.initEcharts()
+			} else {
+				// 动态引入较大类库避免影响页面展示
+				const script = document.createElement('script')
+				// view 层的页面运行在 www 根目录，其相对路径相对于 www 计算
+				script.src = 'static/echarts.js'
+				script.onload = this.initEcharts.bind(this)
+				document.head.appendChild(script)
 			}
 		},
 		methods: {
-
+			initEcharts() {
+				myChart = echarts.init(document.getElementById('echarts'))
+				// 观测更新的数据在 view 层可以直接访问到
+				myChart.setOption(this.option)
+			},
+			updateEcharts(newValue, oldValue, ownerInstance, instance) {
+				// 监听 service 层数据变更
+				if(myChart!=undefined){ myChart.setOption(newValue) }
+			},
+			onClick(event, ownerInstance) {
+				// 调用 service 层的方法
+				ownerInstance.callMethod('onViewClick', {
+					test: 'test'
+				})
+			}
 		}
 	}
 </script>
 
 <style>
-	.container {
-		padding: 20px;
-		font-size: 14px;
-		line-height: 24px;
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.echarts {
+		margin-top: 100px;
+		width: 100%;
+		height: 300px;
 	}
 </style>
--- pages/tabBar/taskList/taskList.vue
+++ pages/tabBar/taskList/taskList.vue
@@ -1,6 +1,11 @@
 <template>
-	<view>
-		
+	<view class="uni-wrap">
+		<uni-section class="mb-10" title="热点任务">
+					<template v-slot:decoration>
+					  <view class="decoration"></view>
+					</template>
+		</uni-section>
+		<uni-section class="mb-10" title="基础任务" type="line"></uni-section>
 	</view>
 </template>
 
@@ -8,15 +13,41 @@
 	export default {
 		data() {
 			return {
-				
 			}
 		},
+		onReady() {
+
+		},
 		methods: {
-			
+
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
+  $uni-success: #18bc37 !default;
 
+	.uni-wrap {
+		flex-direction: column;
+		/* #ifdef H5 */
+		height: calc(100vh - 44px);
+		/* #endif */
+		/* #ifndef H5 */
+		height: 100vh;
+		/* #endif */
+		flex: 1;
+	}
+
+	.mb-10 {
+		margin-bottom: 10px;
+	}
+
+  .decoration{
+    width: 6px;
+    height: 6px;
+		margin-right: 4px;
+    border-radius: 50%;
+		background-color: $uni-success;
+  }
 </style>
+
