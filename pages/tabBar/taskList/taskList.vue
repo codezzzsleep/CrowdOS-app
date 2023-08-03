9e7eac8c5b1370846ad23c1420b505a61cf44524
codezzzsleep <codezzzsleep@163.com>
2023-08-03 21:20:00 +0800

    add

--- pages/tabBar/taskList/taskList.vue
+++ pages/tabBar/taskList/taskList.vue
@@ -6,6 +6,11 @@
 					</template>
 		</uni-section>
 		<uni-section class="mb-10" title="基础任务" type="line"></uni-section>
+		<uni-list>
+			<uni-list-item title="开启点击反馈" clickable  @click="onClick" ></uni-list-item>
+			<uni-list-item title="默认 navigateTo 方式跳转页面" link to="/pages/vue/index/index" @click="onClick($event,1)" ></uni-list-item>
+			<uni-list-item title="reLaunch 方式跳转页面" link="reLaunch" to="/pages/vue/index/index" @click="onClick($event,1)" ></uni-list-item>
+		</uni-list>
 	</view>
 </template>
 
